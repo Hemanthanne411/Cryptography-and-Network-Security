7f88b83b7af2725a7ff68593403bae52446c3997
Hemanthanne411 <hemanth.anne101@gmail.com>
2024-06-12 19:20:23 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,17 +3,16 @@ def main():
     key=int(input("Enter the key for encryption : ")) 
     encrypted_text=""
     
-    for i in range(len(text)): 
-        each=text[i]
+    for i in text: 
 
-        if (each.isupper()):
+        if (i.isupper()):
             # chr() -- to convert to alpabet
             # ord() --to convert to integer version of alphabet
             # 65 -- the integer version for capital letter starts from (65-90)
-            encrypted_text+=chr((ord(each)-65 + key)% 26 +65)
-        elif (each.islower()):
+            encrypted_text+=chr((ord(i)-65 + key)% 26 +65)
+        elif (i.islower()):
             # lower case starts from 97-122
-            encrypted_text+= chr((ord(each) + key - 97) % 26 + 97)
+            encrypted_text+= chr((ord(i) + key - 97) % 26 + 97)
     print(f"the encrypted message : {encrypted_text}")
 if __name__=="__main__":
     main()
